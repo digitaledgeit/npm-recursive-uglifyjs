22234636698fe2152d562d3fd14b1dfdc6b3acc7
James Newell <james@digitaledgeit.com.au>
2018-06-06 23:15:39 +1000

    bump, fix spacing and release

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # recursive-uglifyjs
+
 Recursively traverses a directory and uglifies all JavaScript files contained in the directory and its children.
 
 ## Installation
 
-	npm install -g recursive-uglifyjs
+```bash
+npm install -g recursive-uglifyjs
+```
 
 ## Usage
 
-	recursive-uglifyjs ./a/directory/containing/js/files
+```bash
+recursive-uglifyjs ./a/directory/containing/js/files
+```
--- index.js
+++ index.js
@@ -11,101 +11,102 @@ var finder = require('finder-on-steroids');
 function recursiveUglifyJS(directory, callback) {
 
   //find files
-	return finder(directory).files().name('*.js').find(function(err, files) {
-    if (err) return callback(err);
-
-    var
-      maxProcesses    = 4,
-      activeProcesses = 0,
-      fileIndex       = 0,
-      errors          = []
-    ;
-
-    /**
-     * Called when a process is finished
-     * @param   {String}  [file]    The file that caused the process to finish
-     * @param   {Error}   [error]   The error that caused the process to finish
-     */
-    function finished(file, error) {
-
-      //keep a list of errors
-      if (error) {
-        error.file = file;
-        errors.push(error);
-      }
-
-      //end the current process
-      --activeProcesses;
-
-      //if there are no more active processes then notify the user we're finished
-      if (activeProcesses === 0) {
-        callback(errors.length ? errors : null);
-      }
-
-    }
-
-    /**
-     * Called to process the next file
-     */
-    function next() {
-      var file;
-
-      //check there are more files to uglify
-      if (fileIndex >= files.length) {
-        return finished();
-      }
-
-      //get the file name
-      file = files[fileIndex++];
-
-      //get the file source code
-      fs.readFile(file, function(error, source) {
-        if (error) return finished(file, error);
-
-        //convert the buffer to a string
-        source = source.toString();
-
-        //check the file is not empty or only contains whitespace (uglify-js will throw an error)
-        if (source.replace(/\S/, '').length === 0) {
-
-          debug('Skipped empty file: ' + file);
-
-          //uglify the next script
-          return next();
-
+	return finder(directory).files().name('*.js').find().then(
+    files =>  {
+      
+      var
+        maxProcesses    = 4,
+        activeProcesses = 0,
+        fileIndex       = 0,
+        errors          = []
+      ;
+  
+      /**
+       * Called when a process is finished
+       * @param   {String}  [file]    The file that caused the process to finish
+       * @param   {Error}   [error]   The error that caused the process to finish
+       */
+      function finished(file, error) {
+  
+        //keep a list of errors
+        if (error) {
+          error.file = file;
+          errors.push(error);
         }
-
-        //minify the code
-        var code;
-        try {
-           code = uglify.minify(source).code;
-        } catch (error) {
-          return finished(file, error);
+  
+        //end the current process
+        --activeProcesses;
+  
+        //if there are no more active processes then notify the user we're finished
+        if (activeProcesses === 0) {
+          callback(errors.length ? errors : null);
         }
-
-        //write the code back to the file
-        fs.writeFile(file, code, function(error) {
+  
+      }
+  
+      /**
+       * Called to process the next file
+       */
+      function next() {
+        var file;
+  
+        //check there are more files to uglify
+        if (fileIndex >= files.length) {
+          return finished();
+        }
+  
+        //get the file name
+        file = files[fileIndex++];
+  
+        //get the file source code
+        fs.readFile(file, function(error, source) {
           if (error) return finished(file, error);
-
-          debug('Uglified: ' + file);
-
-          //uglify the next script
-          next();
-
+  
+          //convert the buffer to a string
+          source = source.toString();
+  
+          //check the file is not empty or only contains whitespace (uglify-js will throw an error)
+          if (source.replace(/\S/, '').length === 0) {
+  
+            debug('Skipped empty file: ' + file);
+  
+            //uglify the next script
+            return next();
+  
+          }
+  
+          //minify the code
+          var code;
+          try {
+            code = uglify.minify(source).code;
+          } catch (error) {
+            return finished(file, error);
+          }
+  
+          //write the code back to the file
+          fs.writeFile(file, code, function(error) {
+            if (error) return finished(file, error);
+  
+            debug('Uglified: ' + file);
+  
+            //uglify the next script
+            next();
+  
+          });
+  
         });
-
-      });
-
-		}
-
-    //start processes
-    activeProcesses = maxProcesses;
-    for (var j=0; j<maxProcesses; ++j) {
-      next()
-    }
-
-	});
-
+  
+      }
+  
+      //start processes
+      activeProcesses = maxProcesses;
+      for (var j=0; j<maxProcesses; ++j) {
+        next()
+      }
+  
+    },
+    error => callback(error)
+  );
 }
 
 module.exports = recursiveUglifyJS;
